ef4ad4dcef1dc30d9fdb06b8b8e5fe1a187def42
AdelsonMartinez <adelsonmartinez@hotmail.com>
2024-08-11 12:31:06 -0400

    Delete # Obtener número N del usuario y devolver suma.py

--- # Obtener número N del usuario y devolver suma.py
+++
@@ -1,6 +0,0 @@
-# Obtener número n del usuario
-n = int(input("Introduce un número natural n: "))
-# Calcular la suma de los primeros n números naturales
-suma = n * (n + 1) // 2
-# Mostrar el resultado
-print("La suma de los primeros", n, "números naturales es:", suma)
