a8a264be5608ca448058bee71fa238234baa8f8e
AdelsonMartinez <adelsonmartinez@hotmail.com>
2024-08-11 12:28:33 -0400

    Add files via upload

---
+++ # Obtener número N del usuario y devolver suma.py
@@ -0,0 +1,6 @@
+# Obtener número n del usuario
+n = int(input("Introduce un número natural n: "))
+# Calcular la suma de los primeros n números naturales
+suma = n * (n + 1) // 2
+# Mostrar el resultado
+print("La suma de los primeros", n, "números naturales es:", suma)
