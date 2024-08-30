c7e9be76657b57e98ea33837f361ec78e7fe5b93
Adelson Martinez <adelsonmartinez@hotmail.com>
2024-08-29 20:06:28 -0400

    new file:   Surtidora ITLA Santiago SRL.py

---
+++ Surtidora ITLA Santiago SRL.py
@@ -0,0 +1,14 @@
+#Surtidora ITLA Santiago SRL. 
+from time import localtime, strftime
+
+class Producto:
+    def __init__(self, id_producto, nombre, precio, cantidad_stock, tipo_impuesto):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad_stock = cantidad_stock
+        self.tipo_impuesto = tipo_impuesto
+        
+    def __str__(self):
+        return f"{self.id_producto}\t{self.nombre.ljust(12)}RD${self.precio:.2f} (Stock: {self.cantidad_stock})"
+
