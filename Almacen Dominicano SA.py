c3fdc482abf01d8b597573ba3069cb1cd7232e9b
Adelson Martinez <adelsonmartinez@hotmail.com>
2024-09-03 20:25:19 -0400

    #Se agrega la instruccion para importar el modulo os y se define el método de la clase que se encarga de agregar un ítem.

--- Almacen Dominicano SA.py
+++ Almacen Dominicano SA.py
@@ -1,4 +1,5 @@
-#Se agrega la clase Item y sus atributos
+#Se agrega la clase Item y sus atributos 
+import os
 class Item: 
     def __init__(self, item_id, description, volumen):
         self.item_id = item_id
@@ -18,3 +19,11 @@ class Almacen:
         self.items = []
         self.capacidad_total = altura * anchura * longitud
         self.capacidad_disponible = self.capacidad_total * 0.8  
+    #Se define el método de la clase que se encarga de agregar un ítem
+    def agregar_item(self, item):
+        if self.capacidad_disponible >= item.volumen:
+            self.items.append(item)
+            self.capacidad_disponible -= item.volumen
+            print(f"Ítem {item.item_id} agregado al almacén {self.nombre}.")
+        else:
+            print(f"No hay suficiente espacio en el almacén {self.nombre} para el ítem {item.item_id}.")
